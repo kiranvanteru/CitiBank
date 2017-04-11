15254f9118bd3457cc85d7212a7ec90c6acb847a
kiranvanteru <kk23vanteru@gmail.com>
2017-04-11 20:31:38 +0530

    Citi first

---
+++ CitiBankProject/src/test/java/citiTest1/CitiLoginTest.java
@@ -0,0 +1,21 @@
+package citiTest1;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class CitiLoginTest {
+  @Test
+  public void f() {
+	  
+	  System.out.println("citit test");
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
